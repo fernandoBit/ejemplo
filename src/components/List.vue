842b3b0e99f6e5625e0392ffaea4f292c30f26f3
Fernando Laorden <fernando@bitvalencia.com>
2020-03-04 15:43:02 +0100

    Update List.vue

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-indigo-500 rounded p-3">
+    <div class="bg-indigo-500 rounded p-3 mt-2">
         <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
         <label for="jack">Jack</label>
         <input type="checkbox" id="john" value="John" v-model="checkedNames">
@@ -7,12 +7,10 @@
         <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
         <label for="mike">Mike</label>
         <br>
-        <span>Checked names: {{ checkedNames }}</span><br>
-
-        <select v-model="selected" id="letras">
-            <option v-for="(option,id) in options" :key="id">{{ option.text}}</option>
-        </select>
-        <span>Selected: {{ selected }}</span>
+        <select v-model="selected">
+            <option v-for="(option,id) in options" :key="id" >{{ option.text}}</option>
+        </select><br>
+        <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" v-on:click="send" > Enviar</button>
     </div>
 </template>
 
@@ -23,10 +21,17 @@ export default {
             checkedNames: [], 
             selected: [],
             options: [
-                { text: "A", value: "1"},
-                { text: "B", value: "2"},
-                { text: "C", value: "3"}
-            ]
+                { text: "A", value: 1},
+                { text: "B", value: 2},
+                { text: "C", value: 3}
+            ],
+            count: 0
+        }
+    },methods:{
+        send (){
+            for(var i=0; i<this.checkedNames.length; i++) {
+                console.log(this.checkedNames[i])
+            }
         }
     }
 }
