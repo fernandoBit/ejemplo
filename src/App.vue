b66823f3bb7dc7584a174146cfa99142f2ec8fac
fernandoBit <61780007+fernandoBit@users.noreply.github.com>
2020-03-04 10:27:58 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="bg-indigo-800 flex items-center justify-center h-screen">
+    <div class="text-white font-bold p-10">
+      <h2 class="text-6xl text-center">Vue TailwindCSS Playground</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    //
+  }
+};
+</script>
