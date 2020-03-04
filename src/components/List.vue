2cc9b926d4adc893377325a98ef344167599109b
Fernando Laorden <fernando@bitvalencia.com>
2020-03-04 14:09:52 +0100

    Update List.vue

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,23 +1,34 @@
 <template>
-<div class="">
+    <div class="bg-indigo-500 rounded p-3">
+        <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+        <label for="jack">Jack</label>
+        <input type="checkbox" id="john" value="John" v-model="checkedNames">
+        <label for="john">John</label>
+        <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+        <label for="mike">Mike</label>
+        <br>
+        <span>Checked names: {{ checkedNames }}</span><br>
 
-    <div class="grid grid-cols-3 md:grid-cols-9 text-center content-center text-white">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div>
+        <select v-model="selected" id="letras">
+            <option v-for="(option,id) in options" :key="id">{{ option.text}}</option>
+        </select>
+        <span>Selected: {{ selected }}</span>
     </div>
-</div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return {
+            checkedNames: [], 
+            selected: [],
+            options: [
+                { text: "A", value: "1"},
+                { text: "B", value: "2"},
+                { text: "C", value: "3"}
+            ]
+        }
+    }
 }
 </script>
 
